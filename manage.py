55ea32c985b461de67303760ba8ca0f9edecceaa
DoyunShin <doyun.shin@gmail.com>
2025-05-18 16:51:16 +0900

    feat: Add nginx support

--- manage.py
+++ manage.py
@@ -15,16 +15,34 @@ if sys.version_info < (3, 6):
     print("Python 3.6+ is required")
     sys.exit(1)
 
-
-DOCKERPATH = Path(DOCKERPATH)
 reserv_port = []
 users = []
 
 #if os.system("docker-compose --version") == 0:   dcommand = "docker-compose"
 #elif os.system("docker compose --version") == 0: dcommand = "docker compose"
-if Popen("docker-compose --version", shell=True, stdout=PIPE, stderr=PIPE).wait() == 0:   dcommand = "docker-compose"
-elif Popen("docker compose --version", shell=True, stdout=PIPE, stderr=PIPE).wait() == 0: dcommand = "docker compose"
+if Popen("docker compose --version", shell=True, stdout=PIPE, stderr=PIPE).wait() == 0: dcommand = "docker compose"
+elif Popen("docker-compose --version", shell=True, stdout=PIPE, stderr=PIPE).wait() == 0:   dcommand = "docker-compose"
 else: raise FileNotFoundError("docker-compose not found")
+
+def nginx():
+    if Path(NGINXPATH).exists():
+        for file in Path(NGINXPATH).iterdir():
+            if file.is_file():
+                file.unlink()
+    else:
+        Path(NGINXPATH).mkdir(parents=True, exist_ok=True)
+    
+    # web: {username}.newbie.sparcs.me
+    # api: {username}.api.newbie.sparcs.me
+    for user in users:
+        apiConf = NGINXCONF.format(domain=f"{user}.api.newbie.sparcs.me", port=user["port8000"])
+        webConf = NGINXCONF.format(domain=f"{user}.newbie.sparcs.me", port=user["port3000"])
+        apiConfPath = Path(NGINXPATH) / f"{user}.api.newbie.sparcs.me.conf"
+        webConfPath = Path(NGINXPATH) / f"{user}.newbie.sparcs.me.conf"
+        apiConfPath.write_text(apiConf)
+        webConfPath.write_text(webConf)
+    
+    print("Nginx config created")
 
 
 def port_usage(port: int):
@@ -64,8 +82,8 @@ def create_three_ports():
             return port1, port2, port3
     
 
-def create(manual_user: str = None):
-    DOCKERPATH.mkdir(exist_ok=True)
+def create(manual_user: str = ""):
+    Path(DOCKERPATH).mkdir(exist_ok=True)
     deployComposePath = Path("docker-compose.deploy.yaml")
 
     if deployComposePath.exists():
@@ -82,7 +100,7 @@ def create(manual_user: str = None):
             _add(user)
 
 def _add(name: str):
-    userPath = DOCKERPATH / name
+    userPath = Path(DOCKERPATH) / name
     userPath.mkdir(exist_ok=True)
     userComposePath = userPath / "docker-compose.yaml"
     upass = ''.join(random.choices(string.ascii_letters + string.digits, k=PASSLENGTH))
@@ -112,17 +130,43 @@ def start():
         
         os.system(f"cd {str(ucPath.parent)} && {dcommand} up -d --build")
 
-def getpass():
+def load_info() -> dict:
+    """
+    Load the user information from the docker-compose.yaml file.
+
+    Returns:
+        dict: A dictionary containing the user information.
+
+    Example return:
+    {
+        "user1": {
+            "password": "password1",
+            "port22": 17000,
+            "port3000": 17001,
+            "port8000": 17002
+        },
+        ...
+    }
+    """
+
+    user_info = {}
     for user in Path(DOCKERPATH).iterdir():
         ucPath = user / "docker-compose.yaml"
         if not ucPath.exists():
             raise FileNotFoundError(f"docker compose.yml not found for {user}")
         passwd = ucPath.read_text().split("NEW_PASSWORD=")[1].split("\n")[0]
         portstr = ucPath.read_text().split("ports:")[1].split("volumes")[0].split("- \"")[1:]
-        port1 = portstr[0].split(":")[0]
-        port2 = portstr[1].split(":")[0]
-        port3 = portstr[2].split(":")[0]
-        print(PRINTINFO.format(user=user, passwd=passwd, port22=port1, port3000=port2, port8000=port3))
+        user_info[user.name] = {
+            "password": passwd,
+            "port22": int(portstr[0].split(":")[0]),
+            "port3000": int(portstr[1].split(":")[0]),
+            "port8000": int(portstr[2].split(":")[0])
+        }
+    return user_info
+
+def getpass():
+    for user, info in load_info().items():
+        print(PRINTINFO.format(user=user, passwd=info["password"], port22=info["port22"], port3000=info["port3000"], port8000=info["port8000"]))
 
 def create_menu():
     while True:
@@ -158,7 +202,8 @@ def main():
         print("3. Stop docker-compose")
         print("4. Get passwords")
         print("5. Backup")
-        print("6. Exit")
+        print("6. Create nginx config")
+        print("7. Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
             create_menu()
@@ -171,6 +216,8 @@ def main():
         elif choice == "5":
             backup()
         elif choice == "6":
+            nginx()
+        elif choice == "7":
             break
         else:
             print("Invalid choice")
